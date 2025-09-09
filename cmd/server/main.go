4b19fb6e2ad371e47d862d867cbfb637e2ec6391
Crawcraw <christophe.raucroy@gmail.com>
2025-09-09 18:39:18 +0200

    publish first message

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -16,10 +18,20 @@ func main() {
 	fmt.Println("RabbitMQ connection attempt...")
 	cxn, err := amqp.Dial(cxnStr)
 	if err != nil {
-		log.Fatalf("Couldn't connect to rabbitMQ server: %v", err)
+		log.Fatalf("Couldn't connect to rabbitMQ server: %v\n", err)
 	}
 	defer cxn.Close()
 	fmt.Println("RabbitMQ connection successful.")
+
+	channel, err := cxn.Channel()
+	if err != nil {
+		log.Fatalf("Couldn't create channel: %v\n", err)
+	}
+	err = pubsub.PublishJSON(channel, string(routing.ExchangePerilDirect), string(routing.PauseKey), routing.PlayingState{IsPaused: true})
+	if err != nil {
+		log.Fatalf("Couln't publish message: %v\n", err)
+	}
+	
 
 	// wait for ctrl+c
 	signalChan := make(chan os.Signal, 1)
