bc2fb22cabc869f57b07927a723b8e5fafcfde53
Crawcraw <christophe.raucroy@gmail.com>
2025-09-12 18:51:37 +0200

    implement rabbitmq connection in client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+	connStr := "amqp://guest:guest@localhost:5672/"
+
+	fmt.Println("RabbitMQ connection attempt...")
+	conn, err := amqp.Dial(connStr)
+	if err != nil {
+		log.Fatalf("Couldn't connect to rabbitMQ server: %v\n", err)
+	}
+	defer conn.Close()
+	fmt.Println("RabbitMQ connection successful.")
+
+	name, err := gamelogic.ClientWelcome()
+	if err != nil {
+		log.Fatalf("Unexpected error with name retrieval: %v\n", err)
+	}
+	
+	_, _, err = pubsub.DeclareAndBind(conn, routing.ExchangePerilDirect, fmt.Sprintf("%s.%s", routing.PauseKey, name), routing.PauseKey, pubsub.TransientQueueType)
+	if err != nil {
+		log.Fatalf("Couldn't create channel and queue: %v\n", err)
+	}
+
+	// wait for ctrl+c
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+	fmt.Println("Interruption signal received. Closing connection.")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -8,10 +8,42 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type simpleQueueType string 
+const (
+	DurableQueueType simpleQueueType = "durable"
+	TransientQueueType simpleQueueType = "transient"
+
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	jsonData, err := json.Marshal(val)
 	if err != nil {
 		return fmt.Errorf("error while marshalling val to JSON: %w", err)
 	}
 	return ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{ContentType: "application/json", Body: jsonData})
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType simpleQueueType, // an enum to represent "durable" or "transient"
+) (*amqp.Channel, amqp.Queue, error) {
+		channel, err := conn.Channel()
+		if err != nil {
+			return nil, amqp.Queue{}, fmt.Errorf("error during channel creation: %w", err)
+		}
+
+		queue, err := channel.QueueDeclare(queueName, queueType == "durable", queueType == "transient", queueType == "transient", false, nil)
+		if err != nil {
+			return nil, amqp.Queue{}, fmt.Errorf("error during queue declaration: %w", err)
+		}
+		
+		err = channel.QueueBind(queue.Name, key, exchange, false, nil)
+		if err != nil {
+			return nil, amqp.Queue{}, fmt.Errorf("error during queue binding: %w", err)
+		}
+
+		return channel, queue, nil
 }
